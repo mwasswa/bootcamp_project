e4abcd6fa6a9bafe9afdcdb74dc44b3645e4c4c8
Moses Wasswa <wassmoses@gmail.com>
2017-04-26 12:28:28 +0300

    Adding Unit test for Printing the room

--- app/tests/unittests.py
+++ app/tests/unittests.py
@@ -13,7 +13,8 @@ class SpaceAllocationsTestCases(unittest.TestCase):
                      msg='Should return the right success message if input arguments are proper')
 
 
-    def test_print_room(self):pass
+    def test_print_room(self):
+        self.assertIsInstance([],self.dojo.print_room('Dojo Room 1'),msg='Should return a list of names of occupants')
 
 
     def test_print_allocations(self):pass
@@ -23,24 +24,3 @@ class SpaceAllocationsTestCases(unittest.TestCase):
 
 
     def test_raises_error_for_missing_person_name(self):pass
-
-    def test_prime_numbers_in_range_returns_emptylist_if_arg_is_zero(self):
-        self.assertEqual("argument must be an integer greater than 2", prime_numbers_in_range(0),
-                         msg='should return error message if input is zero')
-
-    def test_prime_numbers_in_range_returns_emptylist_if_arg_is_unity(self):
-        self.assertEqual("argument must be an integer greater than 2", prime_numbers_in_range(1),
-                         msg='should return error message if input value is 1')
-
-    def test_prime_numbers_in_range_returns_list_if_arg_is_ten(self):
-        self.assertEqual([2, 3, 5, 7], prime_numbers_in_range(10),
-                         msg='Output must be list of prime numbers')
-
-    def test_prime_numbers_in_range_returns_output_of_type_list_if_arg_isvalid(self):
-        self.assertIsInstance(prime_numbers_in_range(10), list,
-                              msg='Invalid output type. Should be a list')
-
-    def test_prime_numbers_in_range_returns_emptylist_for_negative_arg(self):
-        self.assertEqual("argument must be an integer greater than 2", prime_numbers_in_range(-10),
-                         msg='Should return error message if argument is negative')
-
